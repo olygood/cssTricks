0dd09e1e4170ec8de0d1c7ea709af6ca863deef2
olygood <https://github.com/olygood>
2026-03-15 08:27:33 +0100

    home page csstricks start github

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+// import typescriptLogo from './assets/typescript.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import { setupCounter } from './counter.ts'
+
+//code d'origine fait avec vite
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+// //ici vient le code HTML si je fait avec vite et composant mais c'est nul de coder dans les ``.
+// `
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
